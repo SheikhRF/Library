b51fec1f738e26b0a7e9da31e198a8a6229874e6
Antuuan <antonycheng2007@gmail.com>
2025-01-14 09:13:41 +0000

    install page now works

--- install.php
+++ install.php
@@ -5,6 +5,9 @@ $username = 'root';
 $password= '';
 //note no Database mentioned here!!
 
+$conn = new PDO("mysql:host=$servername", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE DATABASE IF NOT EXISTS library";
 $conn->exec($sql);
 //next 3 lines optional only needed really if you want to go on an do more SQL here!
