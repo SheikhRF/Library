18f6d8537a0546b8efef7dfa9a4d2e0620fab503
SheikhRF <sheikh.rf@oundleschool.org.uk>
2025-01-14 09:06:15 +0000

    mayhap

--- Books.php
+++ Books.php
@@ -10,19 +10,12 @@
   Title:<input type="text" name="title"><br>
   Author:<input type="text" name="author"><br>
   Genre:<input type="text" name="genre"><br>
-  Blurb:<input type="text" name="house"><br>
-  Rating:<input type="text" name="year"><br>
-  <!--Creates a drop down list-->
-  Gender:<select name="gender">
-		<option value="M">Male</option>
-		<option value="F">Female</option>
-	</select>
-	<br>
-  <!--Next 3 lines create a radio button which we can use to select the user role-->
-  <input type="radio" name="role" value="Pupil" checked> Pupil<br>
-  <input type="radio" name="role" value="Teacher"> Teacher<br>
-  <input type="radio" name="role" value="Admin"> Admin<br>
-  <input type="submit" value="Add User">
+  Blurb:<input type="text" name="blurb"><br>
+  Rating:<input type="TINYINT(1)" name="rating"><br>
+
+
+  <input type="file" name="cover" id="cover">
+  <input type="submit" value="Upload Image" name="submit">
 </form>
 <?php
 	include_once('connection.php');
